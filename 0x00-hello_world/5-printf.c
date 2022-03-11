fc52b05576646f005bf50e4bc7ca0e704288a397
SWaseni <sylviawaseni56@gmail.com>
2022-03-11 13:11:44 -0800

    C world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - prints a text
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
