259b56d5b31c6cb4d4c7a370a1f71b3f38c84c47
SWaseni <sylviawaseni56@gmail.com>
2022-03-16 10:31:49 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print (a-x)x10
+ *
+ * Return: Always zero.
+ */
+void print_alphabet_x10(void)
+{
+	int print = 0;
+	char x = 'a';
+
+	while (print < 10)
+	{
+		x = 'a';
+		while (x <= 'z')
+		{
+			_putchar(x);
+			x++;
+		}
+		_putchar('\n');
+
+		print++;
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # alx-low_level_programming
-Coding in C
+Functions, nested loops
