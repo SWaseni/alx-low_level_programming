2d16c0b2a4d4ec534cbd17d458e977f3ffb6b4e1
SWaseni <sylviawaseni56@gmail.com>
2022-03-28 13:37:39 -0700

    More pointers, arrays and strings

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * print_chessboard - prints chessboard.
- * @a: matrix
-*/
+ * _memset - ironnou
+ * @s: pointer to char params
+ * @b: data to change
+ * @n: index
+ * Return: *s
+ */
 
-void print_chessboard(char (*a)[8])
+char *_memset(char *s, char b, unsigned int n)
 {
-	int i, j;
+	unsigned int i;
 
-	for (i = 0; i < 8; i++)
+	for (i = 0; i < n; i++)
 	{
-		for (j = 0; j < 8; j++)
-		{
-			_putchar(*(*(i + a) + j));
-		}
-	_putchar('\n');
+		s[i] = b;
 	}
+	return (s);
 }
